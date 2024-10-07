41d550025ab67bb728e198ecc75ef36123997720
PrasadDanduPD <prasadvarma.2294@gmail.com>
2024-10-07 14:52:38 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
  #Creating IAM role for EKS
- # resource "aws_iam_role" "master" {
-  #  name = "veera-eks-master1"
+ resource "aws_iam_role" "master" {
+    name = "veera-eks-master1"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
